c52505630b44d9d55092c2415344d513847747ee
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2018-10-02 07:24:12 -0400

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package Go_Engine
 
-import "fmt"
+/*the libraries */
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+//---------------//
 
 func main(){
-	fmt.Println("Welcome this is a test")
+
+	strings.Title("Go Engine")//<-------------- i think this is the title
+	reader := bufio.NewReader(os.Stdin)//<------- declaring the user input
+
+	// the welcome screen and menu
+	fmt.Println("Welcome !!")
+	fmt.Println("A")
+	text, _ := reader.ReadString('\n')
+
+	inputVal(text)//<---------------------------- passing the information to the function
+
+}
+
+
+// validating the user input
+func inputVal(input string){
+
+	if input == "Start" || input == "start" {
+
+	}
+
 }
