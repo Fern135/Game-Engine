a8467e74848f2861b9ea6c67fa9cdb29aa0a2213
fernando camblor <dante.camblor@gmail.com>
2018-10-04 01:39:27 -0400

    UPDATE

--- main.go
+++ main.go
@@ -7,33 +7,73 @@ import (
 	"os"
 	"strings"
 )
+
 //---------------//
 
-func main(){
+func main() {
 
-	strings.Title("Go Engine")//<-------------- i think this is the title
-	reader := bufio.NewReader(os.Stdin)//<------- declaring the user input
+	reader := bufio.NewReader(os.Stdin) //<------- declaring the user input
+
+	while true{
+		strings.Title("Go Engine")//<-------------- i think this is the title. i have not compiled this yet. 
 
 	// the welcome screen and menu
 	fmt.Println("Welcome !!")
-	fmt.Println("A")
+	fmt.Println("A: Start a new project")
+	fmt.Println("B: Options")
+	fmt.Println("C: Graphics Modifier")
+	fmt.Println("D: Close")
 	text, _ := reader.ReadString('\n')
 
-	inputVal(text)//<---------------------------- passing the information to the function
-
+	inputVal(text) //<---------------------------- passing the information to the function
+	}
 }
 
-
 // validating the user input
-func inputVal(input string){
+func inputVal(input string) {
 
+	// user input validation 
 	if input == "A" || input == "a" {
 		start()
+	} else if input == "B" || input == "b" {
+		userOptions()
+	} else if input == "C" || input == "c" {
+		graphicsMod()
+	} else if input == "D" || input == "d" {
+		exit()
+	} else {
+		fmt.Println("Not one of the options")
 	}
 
 }
 
-func start(){
+// start the main app for loading the tools
+func start() {
 	fmt.Println("This is for the start menu ")
-	fmt.Println("This is another test")
+	tmp()
+}
+
+// closing the menu screen
+func exit() {
+
+}
+
+// the graphics usage modifier of the entire game engine
+func graphicsMod() {
+	tmp()
+}
+
+// the options for the modifying in the entire game engine
+func userOptions() {
+	tmp()
+}
+
+// checking if any new projects exist
+func checkIfProjectExist() {
+	tmp()
+}
+
+// temporary hold for the incoming functions. will try to not use OOP 
+func tmp() {
+	fmt.Println("Temp")
 }
